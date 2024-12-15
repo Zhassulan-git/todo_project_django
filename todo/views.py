ec01d11caff93672d16a5e05eba5d4e1d2d59665
Zhassulan-git <57486734+Zhassulan-git@users.noreply.github.com>
2024-12-15 23:43:56 +0500

    adding basic authentication and log out with creating users

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+
+
+@login_required(login_url='/accounts/login')
+def home(request):
+    return render(request, "todo/home.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login')
